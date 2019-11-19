7d4485f8fc4e93e76174d213198d41203d1989e1
Youssef Abada <youssef.abada.x@gmail.com>
2019-11-19 10:55:45 +0100

    converter

---
+++ main.js
@@ -0,0 +1,6 @@
+function convert(){
+    let decimal = document.getElementById("decimal").value;
+    document.getElementById("binary").innerHTML = parseInt(decimal).toString(2);
+    document.getElementById("octal").innerHTML = parseInt(decimal).toString(8);
+    document.getElementById("hexadecimal").innerHTML = parseInt(decimal).toString(16);
+}
