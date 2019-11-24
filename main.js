59ffe209c80b8c2cd3a31b7a8e8a2f4e269ed2a6
Youssef Abada <youssef.abada.x@gmail.com>
2019-11-25 00:45:12 +0100

    upgrade to more flixeble styled system convertor

--- main.js
+++ main.js
@@ -1,39 +1,59 @@
-function convertDecimal(){
-    let decimal = document.getElementById("decimal").value;
-    document.getElementById("decimalToBinary").innerHTML = decToAllSys(decimal, 2); //parseInt(decimal).toString(2);
-    document.getElementById("decimalToOctal").innerHTML = decToAllSys(decimal, 8); //parseInt(decimal).toString(8);
-    document.getElementById("decimalToHexadecimal").innerHTML = decToAllSys(decimal, 16); //parseInt(decimal).toString(16);
-}
+document.getElementById("number").focus();
 
-function decToAllSys(decimal, base){
-    let Reals = [];
-    let Floats = [];
-    let realPart = Math.floor(decimal);
-    let fractPart = ValidFloat(decimal);
-    let i = 0;
-    let hexadecimals = {10 : 'A', 11 : 'B', 12 : 'C',  13 : 'D',  14 : 'E',  15 : 'F'}
-
-    while(realPart){
-        Reals[i] = (realPart % base) >= 10 ? hexadecimals[realPart % base] : realPart % base;
-        realPart = Math.floor(realPart / base);
-        i++;
-    }
-    i = 0;
-    while(fractPart != 0.0){
-        Floats[i] = Math.floor(fractPart * base) >= 10 ? hexadecimals[Math.floor(fractPart * base)] : Math.floor(fractPart * base);
-        fractPart = ValidFloat(fractPart * base);
-        i++;
-        if(i > 4) break;
-    }
-
-    let real = (Reals.length > 0) ? Reals.reverse().toString().replace(/,/g,'') : '0';
-    let float = (Floats.length > 0) ? "." + Floats.toString().replace(/,/g,'') : '';
-    console.log(Floats.length);
-    return real + float;
+function convert(event){
+    let value = document.getElementById("number").value;
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    document.getElementById("result").innerHTML = decToAllSys(BaseToDec(value,parseInt(from)),parseInt(to));
 }
 
 function ValidFloat(decimal){
     let FloatingLength = decimal.toString().replace(/([0-9]*?\.)/g,'').length;
     let floatingPoints = (decimal % 1.0).toFixed(FloatingLength);
     return floatingPoints;
+}
+
+function decToAllSys(decimal, base){
+    let format = '';
+    let realPart = Math.floor(decimal);
+    let fractPart = ValidFloat(decimal);
+    let i = 0;
+    let hexadecimals = {10 : 'A', 11 : 'B', 12 : 'C',  13 : 'D',  14 : 'E',  15 : 'F'};
+    while(realPart){
+        format += (realPart % base) >= 10 ? hexadecimals[realPart % base] : realPart % base;
+        realPart = Math.floor(realPart / base);
+    }
+    format = (format.length > 0) ? format.split('',format.length).reverse().toString().replace(/,/g,'') : '0';
+    while(fractPart != 0.0){
+        if(i === 0) format += '.';
+        fractPart = fractPart * base;
+        format += Math.floor(fractPart) >= 10 ? hexadecimals[Math.floor(fractPart)] : Math.floor(fractPart);
+        fractPart = ValidFloat(fractPart);
+        i++;
+        if(i > 16) break;
+    }
+    return format;
+}
+function BaseToDec(number, base){
+    number = number.toString();
+    let realPart = number;
+    let fractPart = 0;
+    let hexadecimals = {'A' : '10', 'B' : '11', 'C' : '12',  'D' : '13',  'E' : '14',  'F' : '15'};
+    let total = 0;
+
+    if(number.indexOf(".") != -1){
+        realPart = number.split(".")[0]; 
+        fractPart = number.split(".")[1].split('');
+    }
+    for(let i = 0; i < realPart.length ; i++){
+        let currentBit = realPart.length - 1 - i;
+        var factor = (hexadecimals[realPart[currentBit]] != undefined) ?  hexadecimals[realPart[currentBit]] : realPart[currentBit];
+        total += Math.pow(base, i) * parseInt(factor);
+    }
+    for(let i = 0; i < fractPart.length; i++){
+        fractPart[i] = (hexadecimals[fractPart[i]] != undefined) ? hexadecimals[fractPart[i]] : fractPart[i];
+        total += Math.pow(base, -i-1) * parseInt(fractPart[i]);
+        if(i > 16) break;
+    }
+    return total;
 }
